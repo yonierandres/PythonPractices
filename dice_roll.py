31d4bb2c0aadf6eb4ceec35e5ad0de5951058d36
yonier <yonier.andresng@gmail.com>
2021-06-06 13:01:29 -0500

    deleted name in print

--- dice_roll.py
+++ dice_roll.py
@@ -33,7 +33,7 @@ def run():
 
     else:
         print("why you don't want to play? are you afraid",player1_name, "?")
-        print("you'll got lucky today", player1_name)
+        print("you'll got lucky today")
         
 if __name__ == "__main__":
     run()
